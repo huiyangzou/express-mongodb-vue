3feb09932557354a980a15426966c7618c403eaf
luopeng <peng.luo@junyaokc.com>
2020-08-21 13:24:23 +0800

    a

--- admin-api/api.js
+++ admin-api/api.js
@@ -191,8 +191,9 @@ const Api = function(){
 
     app.get('/v1/android', async function(req, res){
         try {
-            var param={_id:req.query._id};
+            var param={_id:req.query._id,typeOne:req.query.typeOne};
             param= _.pickBy(param, _.isString);
+            console.log(JSON.stringify(param))
             let result = await android.list(param);
             res.status(200).send({code:1000,message: 'ok', data: result});
         } catch (error) {
