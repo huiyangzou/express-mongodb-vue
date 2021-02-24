0e5ecd9c01beddb59af87b9c75bfe37cbc6bbdc2
crobot <crobot@crobotdeMacBook-Pro.local>
2021-02-25 01:06:40 +0800

    a

--- adminEle/src/main.js
+++ adminEle/src/main.js
@@ -45,7 +45,7 @@ Vue.prototype.$delete = del;
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数
 const router = new VueRouter({
-    mode: 'history',
+    // mode: 'history',
     routes // (缩写) 相当于 routes: routes
 })
 router.beforeEach((to, from, next) => {
